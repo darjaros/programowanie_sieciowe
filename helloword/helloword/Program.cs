bcdfd413a7039043112b2ea126dbcb066acd7951
programowanie_sieciowe <darekjaros94@gmail.com>
2017-11-13 19:38:13 +0100

    lj

---
+++ helloword/helloword/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.Text;
+
+namespace helloword
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("HELLO WORLD");
+       
+            }
+           
+        }
+    }
---
+++ suma/suma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace suma
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("podaj dane:");
+            string dane = Console.ReadLine();
+            byte ak = 0;
+            byte b2 = 0;
+            byte mask = 1;
+            UTF8Encoding encoding = new UTF8Encoding();
+            byte[] buf = encoding.GetBytes(dane);
+            foreach (byte b in buf)
+            {    
+                ak ^= b;
+                Console.WriteLine(b);
+            }
+            for (byte i = 0; i < 8; i++)
+            {
+                mask = (byte) (1 << i);
+                b2 = (byte) (ak & mask);
+                b2 ^= (byte)(b2 >> i);
+                    
+            }
+        }
+    }
+}
