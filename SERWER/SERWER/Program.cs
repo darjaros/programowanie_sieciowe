6d423f3d55dd53b263559ba16b1edc28985fdd62
darjaros <32834503+darjaros@users.noreply.github.com>
2017-12-11 18:21:13 +0100

    Add files via upload

---
+++ SERWER/SERWER/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;  
+using System.Net.Sockets;  
+using System.Text;  
+
+public class TcpTimeServer {  
+
+    private const int portNum = 13;  
+
+    public static int Main(String[] args) {  
+        bool done = false;  
+
+        TcpListener listener = new TcpListener(portNum);  
+
+        listener.Start();  
+
+        while (!done) {  
+            Console.Write("Waiting for connection...");  
+            TcpClient client = listener.AcceptTcpClient();  
+
+            Console.WriteLine("Connection accepted.");  
+            NetworkStream ns = client.GetStream();  
+
+            byte[] byteTime = Encoding.ASCII.GetBytes(DateTime.Now.ToString());  
+
+            try {  
+                ns.Write(byteTime, 0, byteTime.Length);  
+                ns.Close();  
+                client.Close();  
+            } catch (Exception e) {  
+                Console.WriteLine(e.ToString());  
+            }  
+        }  
+
+        listener.Stop();  
+
+        return 0;  
+    }  
+
+}  
---
+++ client/client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;  
+using System.Net.Sockets;  
+using System.Text;  
+
+public class TcpTimeClient {  
+    private const int portNum = 13;  
+    private const string hostName = "192.168.2.3";  
+
+    public static int Main(String[] args) {  
+        try {  
+            TcpClient client = new TcpClient(hostName, portNum);  
+
+            NetworkStream ns = client.GetStream();  
+
+            byte[] bytes = new byte[1024];  
+            int bytesRead = ns.Read(bytes, 0, bytes.Length);  
+
+            Console.WriteLine(Encoding.ASCII.GetString(bytes,0,bytesRead));  
+
+            client.Close();  
+
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+
+        return 0;  
+    }  
+}
